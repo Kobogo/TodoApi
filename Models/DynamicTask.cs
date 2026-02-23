7b6d374c884240bac23079c810342afab1a14f1e
[adam bang] <39533056+Kobogo@users.noreply.github.com>
2026-02-23 09:29:32 +0100

    refactor: Add Column attributes to properties in DynamicTask and StaticTask models for database mapping

--- Models/DynamicTask.cs
+++ Models/DynamicTask.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TodoApi.Models
 {
@@ -9,14 +10,31 @@ namespace TodoApi.Models
         [Key]
         public int Id { get; set; }
 
+        [Column("userId")]
         public int UserId { get; set; }
+
+        [Column("title")]
         public required string Title { get; set; }
+
+        [Column("isCompleted")]
         public bool IsCompleted { get; set; }
+
+        [Column("timeOfDay")]
         public TimeSpan? TimeOfDay { get; set; }
+
+        [Column("lastCompletedDate")]
         public DateTime? LastCompletedDate { get; set; }
+
+        [Column("lastShownDate")]
         public DateTime? LastShownDate { get; set; }
+
+        [Column("repeatDays")]
         public List<DayOfWeek>? RepeatDays { get; set; }
+
+        [Column("points")]
         public int Points { get; set; } = 2;
+
+        [Column("bonusMinutesEarnedToday")]
         public int? BonusMinutesEarnedToday { get; set; } = 15;
     }
 }
--- Models/Family.cs
+++ Models/Family.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TodoApi.Models
 {
     public class Family
     {
         public int Id { get; set; }
+
+        [Column("familyName")]
         public string FamilyName { get; set; } = string.Empty;
+
+        [Column("createdAt")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         // Relation: En familie har mange brugere
