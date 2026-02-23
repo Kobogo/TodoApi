a0f446260d53e996acd4f4e57ac17eb786ccfeee
[adam bang] <39533056+Kobogo@users.noreply.github.com>
2026-02-23 09:10:05 +0100

    refactor: Rename BonusMinutes to BonusMinutesEarnedToday in DynamicTask and StaticTask models; update task logging logic accordingly

--- Controllers/StaticTasksController.cs
+++ Controllers/StaticTasksController.cs
@@ -76,7 +76,7 @@ namespace TodoApi.Controllers
                     task.IsCompleted = true;
                     task.LastCompletedDate = DateTime.UtcNow;
                     if (task.UserId.HasValue) {
-                        await EnsureTaskLoggedAndAddBonus(task.UserId.Value, 1, task.Points, task.BonusMinutes ?? 0);
+                        await EnsureTaskLoggedAndAddBonus(task.UserId.Value, 1, task.Points, task.BonusMinutesEarnedToday ?? 0);
                     }
                 }
                 else if (!isCompleted) {
